5c539d863d4584c978bed373b576321c3475d6fc
git-addy <a.furtado@gmx.de>
2020-11-09 17:22:04 +0100

    Thie is the first commit that includes a fully functioning and responsive Navbar. Needs further tweaking though. The commit includes Button and Navbar Components that can be reused

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,30 @@
+const MenuItems = [
+    {
+        title: 'AboutMe',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Blog',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gear',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Signup',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
+
+export default MenuItems
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import MenuItems from './MenuItems'
+import './Navbar.css'
+import Button from '../Button'
+
+
+
+class Navbar extends Component {
+  // shortcut rconst
+    constructor (props){
+      super(props)
+      this.state = {
+        clicked: false 
+      }
+}
+   
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked})
+    }
+   
+    render() {
+        return (
+            <div>
+                <nav className="NavbarItems">
+                    <h1 className="navbar-logo">Dr-Addy <i class="fas fa-drum"></i></h1>
+                    <div className="menu-icon" onClick={this.handleClick}>
+                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}>
+                        </i>
+                    </div>
+                    <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                        {MenuItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.url}>
+                                      {item.title}
+                                    </a>
+                               </li>
+                            )
+                        })} 
+                    </ul>
+                    <Button>Sign Up</Button>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
